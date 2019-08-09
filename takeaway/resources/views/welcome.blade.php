524a6619069c97db418795edaf852c6bdd73265f
Jamal <arunajamal@yahoo.com>
2019-08-09 21:13:00 +0300

    updating readme file

--- takeaway/resources/views/welcome.blade.php
+++ takeaway/resources/views/welcome.blade.php
@@ -83,29 +83,7 @@
                 <div class="title m-b-md">
                    Takeaway.com
                 </div>
-                <?php
-$data = array(
-            'to' => 'John Doe',
-            'email' => 'doe@example.com',
-            'subject' => 'Hello John- food is ready',
-            'message_html'=> '<p>Dear John</p><p>You Are The Best!</p>',
-            'message_text'=> 'Dear John, You Are The Best!',
-        );
-        //Add Token To Check
-        $data['token'] = 'ABC123-NotRealToken';
-        //JSON encode the message
-        echo json_encode($data);
-                ?>
 
-                <!-- <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div> -->
             </div>
         </div>
     </body>
